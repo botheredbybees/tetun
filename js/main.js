dc66869652787a415fbe416cafcff98e02565d13
botheredbybees <botheredbybees@gmail.com>
2014-05-18 20:40:06 +1000

    first draft - sort of works

--- js/main.js
+++ js/main.js
@@ -1,1 +1,468 @@
+var CueCards=[[["bondia","good morning",0],
+    ["botardi","good afternoon",0],
+    ["bonoiti","good evening, good night",0],
+    ["ate amanyá","see you tomorrow",0],
+    ["ate logu","see you later today",0],
+    ["adeus","goodbye",0],
+    ["diak","good; well; OK",0],
+    ["hosi"," from, originate from",0],
+    ["obrigadu","thank you (man speaking)",0],
+    ["obrigada","thank you (woman speaking)",0]],[
+    ["senyór","sir, mister",0],
+    ["senyora","Madam, Mrs",0],
+    ["tiu","uncle",0],
+    ["tia","aunt",0],
+    ["maun","older brother",0],
+    ["mana","older sister",0],
+    ["alin","younger brother/sister",0],
+    ["hau","I, me",0],
+    ["ita","you (singular respectful)",0],
+    ["nia","he, she, him, her",0]],[
+    ["saa","what",0],
+    ["nebee","where",0],
+    ["hakarak","want",0],
+    ["naran","name",0],
+    ["hamrook","thirsty",0],
+    ["hamlaha","hungry",0],
+    ["kolen, kole","tired",0],
+    ["moras","sick",0],
+    ["deskansa","rest",0],
+    ["hariis","bathe",0]],[
+    ["mai","come, come to",0],
+    ["baa","go, go to",0],
+    ["haan","eat",0],
+    ["hemu","drink",0],
+    ["lakohi, lakoi","don’t want, refuse",0],
+    ["lisensa","excuse me",0],
+    ["deskulpa","sorry",0],
+    ["nada","you’re welcome",0],
+    ["uma","house, building, home",0],
+    ["eskola","school",0]],[
+    ["bee","water",0],
+    ["bele","can, may, be able to, be allowed to",0],
+    ["la","not",0],
+    ["lae","no",0],
+    ["sin","yes",0],
+    ["ka","QUESTION TAG; or",0],
+    ["lian Indonézia","Indonesian language",0],
+    ["Bahasa","Indonesian language",0],
+    ["lisaun","lesson",0],
+    ["liafuan","word, short segment of speech",0]],[
+    ["kompriende","understand",0],
+    ["hatene","know (something)",0],
+    ["aprende","learn",0],
+    ["hatete fali","say it again",0],
+    ["koalia","speak",0],
+    ["neineik","slowly, softly",0],
+    ["loos","true, correct, straight, right",0],
+    ["sala","wrong; error",0],
+    ["saida","what",0],
+    ["lalais","quickly",0]],[
+    ["ita boot","you (singular formal)",0],
+    ["o","you (singular familiar)",0],
+    ["imi","you (plural)",0],
+    ["ami","we, us (excluding ‘you’)",0],
+    ["ita","we, us (including ‘you’)",0],
+    ["sira","they, them",0],
+    ["nee","this",0],
+    ["favór ida","please",0],
+    ["laós","not, indeed not",0],
+    ["halo","do, make",0]],[
+    ["rona","hear, listen to",0],
+    ["haree","see, look at",0],
+    ["lee","read",0],
+    ["pasiar","go for an outing",0],
+    ["halimar","play, relax",0],
+    ["lao","walk",0],
+    ["tuur","sit",0],
+    ["hamriik","stand",0],
+    ["servisu","work, have a job",0],
+    ["hela","live, stay,reside",0]],[
+    ["rádiu","radio",0],
+    ["televizaun","television",0],
+    ["jornál","newspaper",0],
+    ["fraze","sentence",0],
+    ["depois","and then",0],
+    ["tenki","must, have to",0],
+    ["iha","in, at",0],
+    ["iha nebee?","where?",0],
+    ["iha nee","here",0],
+    ["iha nebaa","there",0]],[
+    ["uluk","formerly, in the past",0],
+    ["horiseik","yesterday",0],
+    ["ohin loron","today",0],
+    ["aban","tomorrow",0],
+    ["aban-bainrua","in the future",0],
+    ["loron","day",0],
+    ["kalan","night",0],
+    ["loron-kalan","day and night",0],
+    ["dadeer","morning (to about 11am)",0],
+    ["meiudia","midday",0]],[
+    [" lokraik, loraik","afternoon (about 2.30-dark)",0],
+    ["ohin","just now, earlier today",0],
+    ["agora","now",0],
+    ["orsida","soon, later today",0],
+    ["hori-kalan","last night, yesterday evening",0],
+    ["orsida kalan","this evening",0],
+    ["bainhira","when? (for future)",0],
+    ["hori-bainhira","when? (for past)",0],
+    ["sedu","early",0],
+    ["tardi","late",0]],[
+    ["ho oras","on time",0],
+    ["nee","this, these, the",0],
+    ["falta","absent, lack",0],
+    ["liu","go past, further",0],
+    ["sura","count",0],
+    ["hira","how many, how much?",0],
+    ["tinan hira","how old (in years)?",0],
+    ["ho","and",0],
+    ["resin","extra, excess",0],
+    ["estuda","study",0]],[
+    ["ida","one",0],
+    ["rua","two",0],
+    ["tolu","three",0],
+    ["haat","four",0],
+    ["lima","five",0],
+    ["neen","six",0],
+    ["hitu","seven",0],
+    ["walu","eight",0],
+    ["sia","nine",0],
+    ["sanulu","ten",0]],[
+    ["sanulu resin ida","eleven",0],
+    ["sanulu resin rua","twelve",0],
+    ["sanulu resin tolu","thirteen",0],
+    ["sanulu resin haat","fourteen",0],
+    ["sanulu resin lima","fifteen",0],
+    ["sanulu resin neen","sixteen",0],
+    ["sanulu resin hitu","seventeen",0],
+    ["sanulu resin walu","eighteen",0],
+    ["sanulu resin sia","nineteen",0],
+    ["rua-nulu","twenty",0]],[
+    ["tolu-nulu","thirty",0],
+    ["haat-nulu","forty",0],
+    ["lima-nulu","fifty",0],
+    ["neen-nulu","sixty",0],
+    ["hitu-nulu","seventy",0],
+    ["walu-nulu","eighty",0],
+    ["sia-nulu","ninety",0],
+    ["zero","zero",0],
+    ["lima-nulu resin ida","fifty one",0],
+    ["atus ida","one hundred",0]],[
+    ["atus rua","two hundred",0],
+    ["atus ida rua","one hundred and one",0],
+    ["atus ida rua-nulu","one hundred and twenty",0],
+    ["rihun ida","one thousand",0],
+    ["rihun rua","two thousand",0],
+    ["tuku","o’clock",0],
+    ["balu","half, some, part of",0],
+    ["númeru","number",0],
+    ["tinan","year",0],
+    ["fulan","month",0]],[
+    ["semana","week",0],
+    ["oras","hour",0],
+    ["minutu","minute",0],
+    ["bainrua","in two days’time, the day after tomorrow",0],
+    ["baintolu","in three days’ time",0],
+    ["bainhaat","in four days’ time",0],
+    ["bainlima","in five days’ time",0],
+    ["bainneen","in six days’ time",0],
+    ["hori-bainruak","two days ago",0],
+    ["hori-baintolu","three days ago",0]],[
+    ["hori-bainhaat","four days ago",0],
+    ["hori-bainlima","five days ago",0],
+    ["hori-bainneen","six days ago",0],
+    ["hanorin","teach",0],
+    ["hakerek","write",0],
+    ["husu","ask, request",0],
+    ["dehan","say, mean",0],
+    ["konyese","know (someone)",0],
+    ["hasoru","meet",0],
+    ["buka","seek, look for",0]],[
+    ["hetan","find, get, come across",0],
+    ["tama","enter",0],
+    ["sai","exit",0],
+    ["eskola","be educated, attend school",0],
+    ["kapaas","beautiful, lovely",0],
+    ["boot","big, important, adult",0],
+    ["kiik","small",0],
+    ["kolega","friend, colleague, school-mate",0],
+    ["ema","person, people",0],
+    ["livru","book",0]],[
+    ["mestri","male teacher",0],
+    ["mestra","female teacher",0],
+    ["estudante","student (esp. at university)",0],
+    ["kláse","class, grade (of school)",0],
+    ["deit","just",0],
+    ["maibee","but",0],
+    ["tanba","because",0],
+    ["see","who",0],
+    ["kona ba","about (a topic), concerning",0],
+    ["kareta","vehicle, car, bus, truck",0]],[
+    ["mikrolét","minibus",0],
+    ["taksi","taxi",0],
+    ["motór","motorbike",0],
+    ["dalan","path, road, way",0],
+    ["estrada","road, street",0],
+    ["liman","hand, arm",0],
+    ["merkadu","market",0],
+    ["loja","shop",0],
+    ["restorante","restaurant",0],
+    ["igreja","church",0]],[
+    ["kruzamentu","intersection",0],
+    ["sae","climb, ascend; get on, ride in a vehicle",0],
+    ["tuun","descend, get out of a vehicle",0],
+    ["para","stop",0],
+    ["fila","turn, return",0],
+    ["hatudu","show, point to",0],
+    ["hein","wait for",0],
+    ["bolu","summon, ask for, call",0],
+    ["selu","pay, pay for",0],
+    ["lori","bring, take; use; drive",0]],[
+    ["besik","near",0],
+    ["dook","far",0],
+    ["loos","right; straight, true",0],
+    ["karuk","left",0],
+    ["ba","to (preposition)",0],
+    ["i","and (to join clauses)",0],
+    ["no","and (in formal contexts)",0],
+    ["mak","FOCUS MARKER",0],
+    ["inan-aman","parents",0],
+    ["amaa","mother, mum",0]],[
+    ["apaa","father, dad",0],
+    ["oan","child",0],
+    ["maun","older brother",0],
+    ["biin","older sister",0],
+    ["alin","younger brother or sister",0],
+    ["maun-alin","brothers and sisters",0],
+    ["avoo","grandparent",0],
+    ["bei-oan","grandchild",0],
+    ["uma kain","household",0],
+    ["kaben","spouse, husband, wife; marry",0]],[
+    ["katuas-oan","husband",0],
+    ["ferik-oan","wife",0],
+    ["namoradu","boyfriend, fiancé",0],
+    ["namorada","girlfriend, fiancée",0],
+    ["feto","woman, girl, female",0],
+    ["mane","man, boy, male",0],
+    ["katuas","mature man",0],
+    ["ferik","mature woman",0],
+    ["klosan","single person",0],
+    ["kaben nain","married person",0]],[
+    ["hakiak","adopt, raise",0],
+    ["iha","have",0],
+    ["moris","live, be born",0],
+    ["mate","die, dead",0],
+    ["hamutuk","together",0],
+    ["nia","POSSESSIVE MARKER",0],
+    ["see nia ...?","whose......?",0],
+    ["uitoan","a little, a few",0],
+    ["hotu","all",0],
+    ["hotu-hotu","all",0]],[
+    ["loke","open, turn on",0],
+    ["taka","close, turn off, cover",0],
+    ["haruka","command, send",0],
+    ["joga","play, gamble",0],
+    ["hadeer","get up, wake up",0],
+    ["toba","lie down, sleep",0],
+    ["dukur","sleep",0],
+    ["matan dukur","sleepy",0],
+    ["hariis tasi","play in the sea",0],
+    ["nani","swim (to somewhere)",0]],[
+    ["ahi","fire, electricity",0],
+    ["odamatan","door",0],
+    ["misa","Catholic mass",0],
+    ["planu","plan",0],
+    ["tasi ibun","coast, shore, beach",0],
+    ["Domingu","Sunday",0],
+    ["Segunda","Monday",0],
+    ["Tersa","Tuesday",0],
+    ["Kuarta","Wednesday",0],
+    ["Kinta","Thursday",0]],[
+    ["Sesta","Friday",0],
+    ["Sábadu","Saturday",0],
+    ["atu","about to, want to, intend to",0],
+    ["ho","with; and",0],
+    ["sira","PLURAL DEFINITE MARKER",0],
+    ["mos","also",0],
+    ["fasi, fase","wash",0],
+    ["habai","dry in the sun",0],
+    ["hamoos","clean",0],
+    ["estrika","iron (clothes); Noun iron",0]],[
+    ["ajuda, tulun","help, assist",0],
+    ["sosa","buy",0],
+    ["faan","sell",0],
+    ["tein","cook",0],
+    ["foer","dirty",0],
+    ["moos","clean",0],
+    ["labarik","child (to about 15 years)",0],
+    ["bebee","baby",0],
+    ["vizinyu","neighbour",0],
+    ["ospitál","hospital",0]],[
+    ["hariis fatin","bathroom",0],
+    ["sentina","toilet",0],
+    ["ropa","clothes",0],
+    ["osan","money",0],
+    ["atividade","activity",0],
+    ["foin","only just, very recently",0],
+    ["hela","currently (CONTINUOUS)",0],
+    ["nusaa?","what&apos;s up? why?",0],
+    ["fali","again, back",0],
+    ["fila fali","return; again",0]],[
+    ["hahaan","food",0],
+    ["etu","cooked rice",0],
+    ["foos","uncooked husked rice",0],
+    ["naan","meat",0],
+    ["ikan","fish",0],
+    ["modo","vegetables; any cooked dish eaten with rice",0],
+    ["aifuan","fruit",0],
+    ["batar","maize (corn)",0],
+    ["ai farina","cassava",0],
+    ["paun","bread",0]],[
+    ["xá","tea",0],
+    ["kafé","coffee",0],
+    ["hasai"," remove, take out",0],
+    ["hataan","reply, agree",0],
+    ["bosu","full (from eating)",0],
+    ["too","enough",0],
+    ["tan","more, again, additional",0],
+    ["seidauk","not yet",0],
+    ["barak","many,  much",0],
+    ["lai","first (before something else)",0]]];
+//console.log(CueCards.length+" elements in the cue cards array");
+//console.log(CueCards);
+var correctCards = 0;
+var direction = 't2e';
+var currentPage;
+$( document ).ready(function() {
+    var pages = '';
+    currentPage = Math.floor(Math.random() * (CueCards.length + 1));
+    for (var i=0; i<CueCards.length; ++i) {
+        pages += '<li id="p'+i+'"><a href="#" onclick="init('+i+')">'+(i+1)+'</a></li>';
+    }
+    $('.pagination').html(pages);
+    init(currentPage);
+});
 
+function init(page) {
+    if(typeof(page)==='undefined') {
+        page = currentPage;
+    } else {
+        currentPage = page;
+    }
+    $('.pagination li').removeClass('active');
+    $('#p'+page).addClass('active');
+    // Hide the success message
+    $('#successMessage').hide();
+    $('#successMessage').css( {
+    left: '580px',
+    top: '250px',
+    width: 0,
+    height: 0
+    } );
+
+    // Reset the game
+    correctCards = 0;
+    $('#cardPile').html( '' );
+    $('#cardSlots').html( '' );
+
+    var Cards = CueCards[page].slice(0);
+    //console.log(Cards);
+    Cards = shuffle(Cards);
+    //console.log(Cards);
+
+    // Create the pile of shuffled cards
+    var numbers = [];
+    var words = [];
+    for ( var i=0; i<10; i++ ) {
+        if (direction == 't2e') {
+            numbers.push(Cards[i][0]);
+            words.push(Cards[i][1]);
+        } else {            
+            words.push(Cards[i][0]);
+            numbers.push(Cards[i][1]);
+        }
+        
+    }
+    
+    var nums = [];
+    for (i=0; i<10; i++ ) {
+        $('<div>' + numbers[i] + '</div>').attr( 'word', words[i] ).attr( 'id', 'card'+i ).appendTo( '#cardPile' ).draggable( {
+          containment: '#content',
+          stack: '#cardPile div',
+          cursor: 'move',
+          revert: true
+        } );
+        nums.push('<div word="'+words[i]+'">' + words[i] + '</div>');
+    }
+    console.log(nums);
+    nums = shuffle(nums);
+    console.log(nums);
+    // Create the card slots
+
+    for (i=0; i<10; i++ ) {
+        $(nums[i]).appendTo( '#cardSlots' ).droppable( {
+          accept: '#cardPile div',
+          hoverClass: 'hovered',
+          drop: handleCardDrop
+        } );
+    }
+    // reverse the direction for the next round
+    if (direction == 't2e') {
+        direction = 'e2t';
+    } else {
+        direction = 't2e';
+    }
+}
+function handleCardDrop( event, ui ) {
+    var slotNumber = $(this).attr( 'word' );
+    var cardNumber = ui.draggable.attr( 'word' );
+
+    // If the card was dropped to the correct slot,
+    // change the card colour, position it directly
+    // on top of the slot, and prevent it being dragged
+    // again
+
+    if ( slotNumber == cardNumber ) {
+        ui.draggable.addClass( 'correct' );
+        ui.draggable.draggable( 'disable' );
+        $(this).droppable( 'disable' );
+        ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+        ui.draggable.draggable( 'option', 'revert', false );
+        correctCards++;
+    } 
+
+    // If all the cards have been placed correctly then display a message
+    // and reset the cards for another go
+
+    if ( correctCards == 10 ) {
+        $('#successMessage').show();
+        $('#successMessage').animate( {
+          left: '380px',
+          top: '200px',
+          width: '400px',
+          height: '100px',
+          opacity: 1
+        } );
+    }
+ 
+}
+function shuffle(cards) {
+  // shuffle the elements in the array
+  var target, temp;
+  for(var i=0; i<cards.length; ++i) {
+    // for each card, find a random target in the array
+    target=Math.floor((Math.random()*cards.length));
+    // and swap our card with the target card
+    //console.log(typeof(cards[target]));
+    if(typeof(cards[target] == 'object')){
+        temp = cards[target].slice(0);
+        cards[target] = cards[i].slice(0);
+        cards[i] = temp.slice(0);
+    } else {
+        temp = cards[target];
+        cards[target] = cards[i];
+        cards[i] = temp;
+    }
+  }
+  return cards;
+}
